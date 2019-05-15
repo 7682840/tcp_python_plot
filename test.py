22cc3610978d4852ac931184edccfe9b79a736e6
7682840 <7682840@student.swin.edu.au>
2019-05-15 22:52:20 +1000

    Refactored code

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt #library used for plotting
+import datetime
 
 fileToOpen = 'scenario-a-cubic.log'
 ipAddresses = ['172.16.11.67', '172.16.10.65']
@@ -7,12 +8,12 @@ lineValues = []
 timeXVals = []
 cwndYVals = []
 buffYVals = []
-plotIdx = 0
 
 #open file and plot cwnd and send buffer occupancy versus time
 with open(fileToOpen) as f:
     for idx, line in enumerate(f):
-        lineValues.append(line.strip().split(","))
+        # lineVals = line.strip().split(",")
+        lineValues.line.strip().split(",")
 
         #print(lineValues[idx])
 
@@ -21,13 +22,21 @@ with open(fileToOpen) as f:
         # [5] - foreign host address
         # [8] - current cwnd
         # [21] - # of bytes in socket send buffer 
+
+        if idx < 2:
+            continue
+
+        if lineValues[3] == ipAddresses[0]
+        timeXVals.append(lineValues[2])
+        cwndYVals.append(lineValues[8])
+        buffYVals.append(lineValues[21])
         
         if not len(lineValues[idx]) < 2:
             if lineValues[idx][3] in ipAddresses and lineValues[idx][5] in ipAddresses:
                 plotIdx = len(timeXVals)
-                timeXVals.append(lineValues[idx][2])    #time
-                cwndYVals.append(lineValues[idx][8])    #cwnd
-                buffYVals.append(lineValues[idx][21])   #buffer
-
-plt.plot(cwndYVals, 'b', buffYVals, 'r')
+                timeXVals.append(lineValues[plotIdx][2])    #time
+                cwndYVals.append(lineValues[plotIdx][8])    #cwnd
+                buffYVals.append(lineValues[plotIdx][21])   #buffer
+                
+plt.plot(timeXVals, cwndYVals, '-')
 plt.show()
